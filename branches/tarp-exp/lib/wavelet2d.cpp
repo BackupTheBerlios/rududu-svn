55362a02527a2fca8a39f039c37c6d346b05785a
nibo637x <nibo637x@4642c630-1732-0410-9703-f311ac64ccab>
2009-02-23 21:10:45 +0000

    space changed to tab for huffman stats

--- branches/tarp-exp/lib/wavelet2d.cpp
+++ branches/tarp-exp/lib/wavelet2d.cpp
@@ -161,14 +161,14 @@ void CWavelet2D::CodeBand(CMuxCodec * pCodec, int Quant, int lambda)
 #ifdef GENERATE_HUFF_STATS
 	for( int i = 0; i < 17; i++){
 		for( int j = 0; j < 17; j++){
-			cout << CBandCodec::histo_l[i][j] << " ";
+			cout << CBandCodec::histo_l[i][j] << "\t";
 		}
 		cout << endl;
 	}
 	cout << endl;
 	for( int i = 0; i < 17; i++){
 		for( int j = 0; j < 16; j++){
-			cout << CBandCodec::histo_h[i][j] << " ";
+			cout << CBandCodec::histo_h[i][j] << "\t";
 		}
 		cout << endl;
 	}
